7653441750fe9261d237593e3eb24ed8fe24a52d
Masterofowls <danvlad2015@gmail.com>
2024-09-28 21:22:23 +0300

    21

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,12 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('my-pwa-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/styles/global.css',
-          '/pages/_app.js',
-          '/pages/index.js',
-          '/favicon.ico',
-          '/public/images/icon192.jpg',
-          '/public/images/icon512.jpg',
-          // Добавьте другие важные ресурсы
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+// public/sw.js
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetching:', event.request.url);
+});
